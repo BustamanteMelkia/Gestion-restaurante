d1dbe7900e2876992796eaa14c51f443aa9e56f8
Esiel Arizmendi <esiel_kar@hotmail.com>
2020-11-25 01:47:42 -0600

    Update consultas.php

--- inc/funciones/consultas.php
+++ inc/funciones/consultas.php
@@ -29,4 +29,40 @@
             return false;
         }
     }
+
+    //Retorna el total de dinero de los productos de un pedido
+    function getTotalPedido($id_pedido){
+        include("conexion.php");
+        try{
+            $consulta= "SELECT total FROM pedidos WHERE id_pedido=$id_pedido";
+            return $conn->query($consulta);
+        }catch(Exception $e){
+            echo 'Error al cargar los datos de la base de datos';
+            return false;
+        }
+    }
+
+    //Retorna el total de las ventas realizadas
+    function getTotal(){
+        include("conexion.php");
+        try{
+            $consulta= "SELECT SUM(total) FROM pedidos";
+            return $conn->query($consulta);
+        }catch(Exception $e){
+            echo 'Error al cargar los datos de la base de datos';
+            return false;
+        }
+    }
+
+    //Retorna el total de las ventas realizadas en una fecha especifica
+    function getTotalDia($fecha){
+        include("conexion.php");
+        try{
+            $consulta= "SELECT SUM(total) FROM pedidos WHERE fecha=$fecha";
+            return $conn->query($consulta);
+        }catch(Exception $e){
+            echo 'Error al cargar los datos de la base de datos';
+            return false;
+        }
+    }
 ?>
